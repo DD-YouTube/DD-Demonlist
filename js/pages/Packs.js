4e86efdfc1cc6e202fe88e088369344aea10f963
DD-YouTube <clasherdb2024@gmail.com>
2026-06-16 17:01:15 +0200

    Create Packs.js

---
+++ js/pages/Packs.js
@@ -0,0 +1,11 @@
+export default {
+    template: `
+        <main class="page-packs">
+            <h1>Packs</h1>
+            <p>Hier kommen später deine Packs rein.</p>
+        </main>
+    `,
+    data() {
+        return {};
+    }
+};
