19646afcda601cefe6a135db9b9af065cd06607a
DD-YouTube <clasherdb2024@gmail.com>
2026-06-16 18:05:18 +0200

    Update Packs.js

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -1,4 +1,26 @@
 export default {
+    template: `
+        <div class="packs-page">
+            <h1>Packs</h1>
+
+            <div v-if="loading">Loading Packs…</div>
+            <div v-if="error" class="error">{{ error }}</div>
+
+            <div v-if="!loading && !error">
+                <div v-for="pack in packs" :key="pack.name" class="pack">
+                    <h2>{{ pack.name }}</h2>
+                    <p>{{ pack.description }}</p>
+
+                    <ul>
+                        <li v-for="id in pack.levels" :key="id">
+                            {{ getLevelName(id) }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    `,
+
     data() {
         return {
             packs: [],
@@ -10,7 +32,6 @@ export default {
 
     async created() {
         try {
-            // WICHTIG: absoluter Pfad, sonst 404
             const packsResponse = await fetch("/data/packs.json");
             const levelsResponse = await fetch("/data/levels.json");
 
